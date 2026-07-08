cab6a9029a6ce2943e52fec3cacb7586e5910664
VIRAJ2823 <virajbalfe7922@gmail.com>
2026-07-08 21:29:03 +0530

    templates

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask, redirect,url_for,render_template
 
 app = Flask(__name__)
 
-@app.route("/home")
-def home():
-    return render_template("index.html")
+@app.route("/user/<name>")
+def user(name):
+    return render_template("index.html", content=name)
 
 if __name__ == "__main__":
     app.run()
+
+    
