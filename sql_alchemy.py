1b1109b2eedd2b567e8e5056d690cfef1b963d8a
VIRAJ2823 <virajbalfe7922@gmail.com>
2026-07-09 20:08:05 +0530

    db

---
+++ sql_alchemy.py
@@ -0,0 +1,37 @@
+from flask import Flask , redirect , url_for , render_template , request , session , flash
+
+app = Flask(__name__)
+app.secret_key = "mysecret123"
+
+@app.route("/home")
+def home():
+    return render_template("index.html")
+
+@app.route("/login" , methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        user = request.form["nm"]
+        session["user"] = user
+        return redirect(url_for("user", user=user))
+    else:    
+        return render_template("login.html")
+
+@app.route("/<user>")
+def user(user):
+    if "user" in session:
+        user = session["user"]
+        return f"<h1>{user}</h1>"
+    else:
+        return redirect(url_for("login"))
+
+@app.route("/logout")
+def logout():
+    if "user" in session:
+        user = session["user"]
+    session.pop("user", None)
+    flash(f"You have been logged out! {user}", "info")
+    return redirect(url_for("login"))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
