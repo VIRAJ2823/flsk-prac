1048a40948d02e154828728db96d34df06b9f4c1
VIRAJ2823 <virajbalfe7922@gmail.com>
2026-07-09 19:22:44 +0530

    login & logout sessions

--- sessions.py
+++ sessions.py
@@ -24,7 +24,10 @@ def user(user):
     else:
         return redirect(url_for("login"))
 
-
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
 
 if __name__ == "__main__":
     app.run(debug=True)
