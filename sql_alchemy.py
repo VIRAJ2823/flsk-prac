ce205bf3d5fdea3952063e3ecd1a8c48eaa46fd5
VIRAJ2823 <virajbalfe7922@gmail.com>
2026-07-10 19:28:35 +0530

    db vs seesion

--- sql_alchemy.py
+++ sql_alchemy.py
@@ -3,11 +3,9 @@ from flask import Flask, redirect, url_for, render_template, request, session, f
 app = Flask(__name__)
 app.secret_key = "mysecret123"
 
-
 @app.route("/home")
 def home():
     return render_template("index.html")
-
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -15,39 +13,39 @@ def login():
         user = request.form["nm"]
         session["user"] = user
         return redirect(url_for("user", user=user))
-
-    return render_template("login.html")
-
+    else:
+        return render_template("login.html")
 
 @app.route("/<user>", methods=["GET", "POST"])
 def user(user):
-    if "user" not in session:
+    if "user" in session:
+        user = session["user"]
+
+        if request.method == "POST":
+            email = request.form["email"]
+            session["email"] = email
+            flash(f"Email {email} has been saved for user {user}", "success")
+
+        if "email" in session:
+            email = session["email"]
+        else:
+            email = ""
+
+        return render_template("user.html", user=user, email=email)
+
+    else:
         return redirect(url_for("login"))
-
-    user = session["user"]
-
-    if request.method == "POST":
-        email = request.form["email"]
-        session["email"] = email
-        flash(f"Email '{email}' has been saved for {user}.", "success")
-
-    email = session.get("email")
-
-    return render_template("user.html", user=user, email=email)
-
 
 @app.route("/logout")
 def logout():
-    user = session.get("user")
+    if "user" in session:
+        user = session["user"]
 
     session.pop("user", None)
     session.pop("email", None)
-
-    if user:
-        flash(f"You have been logged out! Goodbye {user}.", "info")
-
+    flash(f"You have been logged out! {user}", "info")
     return redirect(url_for("login"))
-
 
 if __name__ == "__main__":
     app.run(debug=True)
+
