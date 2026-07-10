622f0f0bf5b16072922b35b30f4e0f96e4c180c3
VIRAJ2823 <virajbalfe7922@gmail.com>
2026-07-10 19:43:45 +0530

    db

--- sql_alchemy.py
+++ sql_alchemy.py
@@ -1,7 +1,23 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.secret_key = "mysecret123"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+
+db = SQLAlchemy(app)
+
+class Users(db.model):
+    id = db.column(db.integer,primary_key=True)
+    name = db.column(db.string(100))
+    email = db.column(db.string(100))
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
 
 @app.route("/home")
 def home():
