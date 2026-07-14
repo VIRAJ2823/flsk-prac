1e94e04b0f69d3340083ed534e50586d429815f2
VIRAJ2823 <virajbalfe7922@gmail.com>
2026-07-14 19:46:44 +0530

    orm core

--- db_basics.py
+++ db_basics.py
@@ -1,3 +1,4 @@
+
 from sqlalchemy import MetaData, create_engine, text ,  MetaData , Table , Column , Integer , String , Insert
 
 engine = create_engine('sqlite:///mydatabase.db', echo = True)
@@ -18,13 +19,20 @@ conn = engine.connect()
 
 stmt1 = Insert(people).values(id=1, name="viraj", age=25)
 stmt2 = Insert(people).values(id=2, name="nikhil", age=15)
+stmt3 = Insert(people).values(id=3, name="sachin", age=30)
 
 #conn.execute(stmt1)
 #conn.execute(stmt2)
+conn.execute(stmt3)
 #conn.commit()
 
 
-select_statement = people.select().where(people.c.age > 20)
+delete_statement = people.delete().where(people.c.name == "sachin")
+conn.execute(delete_statement)
+conn.commit()       # Save the update
+
+# Now execute a SELECT query
+select_statement = people.select()
 
 result = conn.execute(select_statement)
 
