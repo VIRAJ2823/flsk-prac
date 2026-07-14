d8cc605f39e16bf14f01300df16b11dbdc4b00ed
VIRAJ2823 <virajbalfe7922@gmail.com>
2026-07-14 10:31:22 +0530

    db adv

--- db_basics.py
+++ db_basics.py
@@ -1,9 +1,17 @@
 from sqlalchemy import create_engine, text
 
-engine = create_engine('sqlite:///my_database.db', echo = True)
+engine = create_engine('sqlite:///mydatabase.db', echo = True)
 
 conn = engine.connect()
 
-conn.execute(text("CREATE TABLE IF NOT EXISTS users ( name TEXT, age INTEGER)"))
+conn.execute(text("CREATE TABLE IF NOT EXISTS people ( name TEXT, age INTEGER)"))
 
 conn.commit()
+
+from sqlalchemy.orm import Session
+
+session = Session(engine)
+
+session.execute(text('INSERT INTO PEOPLE (name,age)  VALUES ("VIRAJ", 19)'))
+
+session.commit()
