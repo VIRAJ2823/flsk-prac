6c0704fa8785dea65ca9221fc47ff3c16b331526
VIRAJ2823 <virajbalfe7922@gmail.com>
2026-07-15 14:42:23 +0530

    orm ()

--- main.py
+++ main.py
@@ -1,35 +1,37 @@
-from sqlalchemy import create_engine ,Integer ,String ,Float ,Column ,ForeignKey
-from sqlalchemy.orm import declarative_base , sessionmaker , relationship
+from sqlalchemy import create_engine, Integer, String, Float, Column, ForeignKey
+from sqlalchemy.orm import declarative_base, sessionmaker, relationship
 
-engine = create_engine('sqlite:///mydatabase.db', echo = True)
+engine = create_engine('sqlite:///mydatabase.db', echo=True)
 
 base = declarative_base()
 
 class person(base):
     __tablename__ = 'people'
-    id = Column(Integer,primary_key=True)
-    name = Column(String , nullable=False)
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
     age = Column(Float)
 
-    things = relationship('things',back_populates='person')
-
+    things = relationship('things', back_populates='person')
 
 
 class things(base):
     __tablename__ = 'thing'
-    id = Column(Integer,primary_key=True)
-    description = Column(String , nullable = False)
+
+    id = Column(Integer, primary_key=True)
+    description = Column(String, nullable=False)
     value = Column(Float)
-    owner = Column(Integer, ForeignKey = ('people.id'))
+    owner = Column(Integer, ForeignKey('people.id'))
 
-    person = relationship('people',back_populates='things')
+    person = relationship('person', back_populates='things')
 
 
-base.metadata.Create_all(engine)
+base.metadata.create_all(engine)
+
 Session = sessionmaker(bind=engine)
 session = Session()
 
-new_person = person(name:'sam',age=26)
+new_person = person(name='sam', age=26)
 session.add(new_person)
 
 session.commit()
