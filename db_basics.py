1bc3f7a986156a7786113352a8efca56ba35c813
VIRAJ2823 <virajbalfe7922@gmail.com>
2026-07-15 12:47:42 +0530

    orm core

--- db_basics.py
+++ db_basics.py
@@ -1,5 +1,5 @@
 
-from sqlalchemy import MetaData, create_engine, text ,  MetaData , Table , Column , Integer , String , Insert , Float , ForeignKey
+from sqlalchemy import MetaData, create_engine, text ,  MetaData , Table , Column , Integer , String , Insert , Float , ForeignKey, values
 
 engine = create_engine('sqlite:///mydatabase.db', echo = True)
 
@@ -47,3 +47,22 @@ conn = engine.connect()
 
 # for row in result.fetchall():
 #     print(row)
+
+insert_people = things.insert().values([
+    {'name': 'viraj', 'age': 25},
+    {'name': 'nikhil', 'age': 15},
+    {'name': 'sachin', 'age': 30},
+    {'name': 'yujin', 'age': 20},
+])
+
+insert_things = things.insert(),values([
+    {'owner': 1 , 'description':'laptop', 'value': 1000.0},
+    {'owner':2, 'description': 'phone', 'value': 500.0},
+    {'owner':3, 'description': 'tablet', 'value': 300.0},
+    {'owner':4, 'description': 'watch', 'value': 200.0},
+    {'owner':1, 'description': 'headphones', 'value': 150.0},
+    {'owner':2, 'description': 'keyboard', 'value': 100.0} 
+])
+
+conn.execute(insert_people)
+conn.commit()
