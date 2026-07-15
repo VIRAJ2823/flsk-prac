f02df68cfdfc428f20e9e6f8ba6db8bf75119104
VIRAJ2823 <virajbalfe7922@gmail.com>
2026-07-15 13:43:44 +0530

    orm core

--- db_basics.py
+++ db_basics.py
@@ -1,5 +1,5 @@
 
-from sqlalchemy import MetaData, create_engine, text ,  MetaData , Table , Column , Integer , String , Insert , Float , ForeignKey, values
+from sqlalchemy import MetaData, create_engine, text ,  MetaData , Table , Column , Integer , String , Insert , Float , ForeignKey, values , Function
 
 engine = create_engine('sqlite:///mydatabase.db', echo = True)
 
@@ -48,32 +48,39 @@ conn = engine.connect()
 # for row in result.fetchall():
 #     print(row)
 
-insert_people = people.insert().values([
-    {'name': 'viraj', 'age': 25},
-    {'name': 'nikhil', 'age': 15},
-    {'name': 'sachin', 'age': 30},
-    {'name': 'yujin', 'age': 20},
-])
+# insert_people = people.insert().values([
+#     {'name': 'viraj', 'age': 25},
+#     {'name': 'nikhil', 'age': 15},
+#     {'name': 'sachin', 'age': 30},
+#     {'name': 'yujin', 'age': 20},
+# ])
 
-insert_things = things.insert().values([
-    {'owner': 1 , 'description':'laptop', 'value': 1000.0},
-    {'owner':2, 'description': 'phone', 'value': 500.0},
-    {'owner':3, 'description': 'tablet', 'value': 300.0},
-    {'owner':4, 'description': 'watch', 'value': 200.0},
-    {'owner':1, 'description': 'headphones', 'value': 150.0},
-    {'owner':2, 'description': 'keyboard', 'value': 100.0} 
-])
+# insert_things = things.insert().values([
+#     {'owner': 1 , 'description':'laptop', 'value': 1000.0},
+#     {'owner':2, 'description': 'phone', 'value': 500.0},
+#     {'owner':3, 'description': 'tablet', 'value': 300.0},
+#     {'owner':4, 'description': 'watch', 'value': 200.0},
+#     {'owner':1, 'description': 'headphones', 'value': 150.0},
+#     {'owner':2, 'description': 'keyboard', 'value': 100.0} 
+# ])
 
-conn.execute(insert_people)
-conn.commit()
+# conn.execute(insert_people)
+# conn.commit()
 
-conn.execute(insert_things)
-conn.commit()
+# conn.execute(insert_things)
+# conn.commit()
 
-join_statement = people.join(things, people.c.id  == things.c.owner)
-select_statement = people.select().with_only_columns(people.c.name, things.c.description).select_from(join_statement) 
+# join_statement = people.join(things, people.c.id  == things.c.owner)
+# select_statement = people.select().with_only_columns(people.c.name, things.c.description).select_from(join_statement) 
 
-result = conn.execute(select_statement)
+# result = conn.execute(select_statement)
+
+# for row in result.fetchall():
+#     print(row)
+
+
+group_by_statement = things.select().with_only_columns(things.c.owner, function.sum(things.c.value)).group_by(things.c.owner)
+result = conn.execute(group_by_statement)
 
 for row in result.fetchall():
     print(row)
