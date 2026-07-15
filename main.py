32293d837f6935452f1596cda5ab3806571389b8
VIRAJ2823 <virajbalfe7922@gmail.com>
2026-07-15 14:26:47 +0530

    orm

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
-from sqlalchemy import create_engine ,Interger ,String ,Float ,column
+from sqlalchemy import create_engine ,Integer ,String ,Float ,Column ,ForeignKey
 from sqlalchemy.orm import declarative_base , sessionmaker , relationship
 
 engine = create_engine('sqlite:///mydatabase.db', echo = True)
+
+base = declarative_base()
+
+class person(base):
+    __tablename__ = 'people'
+    id = Column(Integer,primary_key=True)
+    name = Column(String , nullable=False)
+    age = Column(Float)
+
+    things = relationship('thing',back_populates='person')
+
+
+
+class things(base):
+    __tablename__ = 'thing'
+    id = Column(Integer,primary_key=True)
+    description = Column(String , nullable = False)
+    value = Column(Float)
+    owner = Column(Integer, ForeignKey = ('people.id'))
+
+    person = relationship('people',back_populates='things')
