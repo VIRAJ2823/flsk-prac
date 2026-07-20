c6da93f96bddd07239e9dc83f96d1a16bcb33565
VIRAJ2823 <virajbalfe7922@gmail.com>
2026-07-20 09:29:41 +0530

    jinja prac

--- app3.py
+++ app3.py
@@ -4,7 +4,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template(
+        "home.html",
+        name="viraj"
+        )
 
 @app.route("/users")
 def users():
