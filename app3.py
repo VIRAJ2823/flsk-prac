ece0be822b05caf037fcbd5ac338288618071601
VIRAJ2823 <virajbalfe7922@gmail.com>
2026-07-21 14:14:51 +0530

    jinja prac

--- app3.py
+++ app3.py
@@ -2,19 +2,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/home")
 def home():
-    return render_template(
-        "home.html",
-        name="viraj balfe",
+    return render_template("home.html",
+        name="Viraj Balfe",
         age=17,
         college="SPPU",
-        students=[
-            "viraj",
-            "rahul",
-            "akanksha"
-        ]
-    )
+        students=["viraj", "rahul", "akanksha"]   )
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
