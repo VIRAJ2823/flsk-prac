8dee962337fdd4fd7a854340eb2325a1b0a14269
VIRAJ2823 <virajbalfe7922@gmail.com>
2026-07-21 19:23:54 +0530

    forms

--- app3.py
+++ app3.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template ,request ,redirect,url_for
 
 app = Flask(__name__)
 
@@ -10,9 +10,35 @@ def home():
         college="SPPU",
         students=["viraj", "rahul", "akanksha"]   )
 
+
 @app.route("/login")
-def form():
+def login_page():
     return render_template("form.html")
+
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+
+    if request.method == "POST":
+
+        username = request.form["username"]
+        password = request.form["password"]
+
+        if username == "viraj" and password == "12345":
+
+            return redirect(url_for("dashboard"))
+        
+        return "invalid username or password"
+
+        
+    
+
+    return render_template("login.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return"<h1>welcome viraj</h1>"
 
 @app.route("/about")
 def about():
