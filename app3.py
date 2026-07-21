55d85d9fa2928017677cb84390544d8324f822fe
VIRAJ2823 <virajbalfe7922@gmail.com>
2026-07-21 09:46:42 +0530

    jinja prac

--- app3.py
+++ app3.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def home():
     return render_template(
         "home.html",
-        name="Viraj",
+        name="viraj balfe",
         age=17,
         college="SPPU",
         students=[
