ce113021b7156e4a1cd18caab5b6aea7be8392e9
VIRAJ2823 <virajbalfe7922@gmail.com>
2026-07-22 15:14:11 +0530

    auth basic

--- app4.py
+++ app4.py
@@ -1,5 +1,6 @@
-from flask import Flask
-from models import db
+from flask import Flask, render_template, request, redirect, flash
+from models import db, User
+from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
 
@@ -18,5 +19,24 @@ with app.app_context():
 def home():
     return "<h1>Authentication Project</h1>"
 
+@app.route("/register", methods=["POST"])
+def register():
+
+    username = request.form["username"]
+    password = request.form["password"]
+
+    hashed_password = generate_password_hash(password)
+
+    new_user = User(
+        username=username,
+        password=hashed_password
+    )
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    flash("Registration Successful!")
+
+    return redirect("/register")   
 if __name__ == "__main__":
     app.run(debug=True)
