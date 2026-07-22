d880a864ef6c3ccfcc81ff305355549abbd07401
VIRAJ2823 <virajbalfe7922@gmail.com>
2026-07-22 15:34:01 +0530

    auth basic

--- app4.py
+++ app4.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, flash, session
 from models import db, User
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash , check_password_hash
 
 app = Flask(__name__)
 
@@ -18,6 +18,11 @@ with app.app_context():
 @app.route("/")
 def home():
     return "<h1>Authentication Project</h1>"
+
+@app.route("/register", methods=["GET"])
+def register_page():
+    return render_template("register.html")
+
 
 @app.route("/register", methods=["POST"])
 def register():
@@ -38,5 +43,31 @@ def register():
     flash("Registration Successful!")
 
     return redirect("/register")   
+
+@app.route("/login", methods=["GET"])
+def login_page():
+    return render_template("login.html")
+
+@app.route("/login", methods=["POST"])
+def login():
+
+    username = request.form["username"]
+    password = request.form["password"]
+
+    user = User.query.filter_by(username=username).first()
+
+    if user and check_password_hash(user.password, password):
+
+        session["user"] = username
+
+        flash("Login Successful!")
+
+        return redirect("/dashboard")
+
+    flash("Invalid Username or Password!")
+
+    return redirect("/login")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
