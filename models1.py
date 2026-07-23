bdf8885de6fb814aaa53a8052d9691b07a25b9b9
VIRAJ2823 <virajbalfe7922@gmail.com>
2026-07-23 19:07:17 +0530

    relationship

--- models1.py
+++ models1.py
@@ -9,6 +9,23 @@ class Student(db.Model):
 
     name = db.Column(db.String(100), nullable=False)
 
+    attendances = db.relationship(
+        "Attendance",
+        back_populates="student"
+    )
+
+
+class Lecture(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    subject = db.Column(db.String(100), nullable=False)
+
+    attendances = db.relationship(
+        "Attendance",
+        back_populates="lecture"
+    )
+
 
 class Attendance(db.Model):
 
@@ -19,9 +36,19 @@ class Attendance(db.Model):
         db.ForeignKey("student.id")
     )
 
+    lecture_id = db.Column(
+        db.Integer,
+        db.ForeignKey("lecture.id")
+    )
+
     status = db.Column(db.String(20))
 
     student = db.relationship(
         "Student",
-        backref="attendances"
+        back_populates="attendances"
+    )
+
+    lecture = db.relationship(
+        "Lecture",
+        back_populates="attendances"
     )
