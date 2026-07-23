d73d80d28a9dfe4ad98350f83efeee135388697d
VIRAJ2823 <virajbalfe7922@gmail.com>
2026-07-23 09:47:01 +0530

    auth basic

--- forms.py
+++ forms.py
@@ -3,3 +3,23 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 
 from wtforms.validators import DataRequired, Length
+
+class RegisterForm(FlaskForm):
+
+    username = StringField()
+
+    password = PasswordField()
+
+    submit = SubmitField()
+
+username = StringField(
+    "Username",
+    validators=[DataRequired()]
+)
+
+
+password = PasswordField(
+    "Password",
+    validators=[DataRequired(), Length(min=8)]
+)
+
