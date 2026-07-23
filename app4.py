e72a3515e3705ed347571f4d824f2dc0e6af7d24
VIRAJ2823 <virajbalfe7922@gmail.com>
2026-07-23 09:35:56 +0530

    auth basic

--- app4.py
+++ app4.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, flash, session
 from models import db, User
 from werkzeug.security import generate_password_hash , check_password_hash
 
+
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
